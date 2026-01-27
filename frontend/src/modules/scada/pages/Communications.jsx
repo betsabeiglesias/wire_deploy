13db3595d60c5c8c1c47e264ecbe4b7de5b0eb91
Aroa <aroa.rdtingenieros@gmail.com>
2026-01-27 12:55:09 +0100

    solucionado problema ruta /devices y vuelta a crear ConfigurarComunicacionesButton

--- frontend/src/modules/scada/pages/Communications.jsx
+++ frontend/src/modules/scada/pages/Communications.jsx
@@ -19,6 +19,7 @@ import { NeedleGauge } from "../components/gauges/NeedleGauge";
 import { Filters } from "../components/Filters";
 import { GatewayTable } from "../components/GatewayTable";
 import HomeButton from "../../../components/HomeButton";
+import ConfigurarComunicacionesButton from "../components/CommunicationsConfigButton";
 
 export default function Communications() {
   const { connected, dataStale, allTags } = useGatewayData();
