35dec56635e8d5a020f7fe7263fbe79fff833513
JonY-Vinan <jony.v.c.ecuador95@gmail.com>
2026-02-03 15:24:21 +0100

    Merge sin conflictos

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
 
 // /**
 //  * Realiza el login y devuelve los datos
