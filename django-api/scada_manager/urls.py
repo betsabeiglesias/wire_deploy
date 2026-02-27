e7711c182e2f90eb2af95762af8152691837b7bb
Aroa <aroa.rdtingenieros@gmail.com>
2026-02-27 14:47:27 +0100

    emilinada tabla mylayout que no se utilizaba y cambios en back para poder arrastrar los hmis y que persistan. ya funciona. pte hacer lo mismo con los pbi

--- django-api/scada_manager/urls.py
+++ django-api/scada_manager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import save_layout, my_layouts, layout_detail
+from .views import save_layout, my_layouts, layout_detail, reorder_layouts
 
 app_name = 'scada_manager'
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path("save-layout/", save_layout, name="save-layout"),
     path("my-layouts/", my_layouts, name="my_layouts"),
     path("layout/<int:title_id>/", layout_detail, name="layout_detail"),
+    path("reorder-layouts/", reorder_layouts, name="reorder_layouts"),
 ]
