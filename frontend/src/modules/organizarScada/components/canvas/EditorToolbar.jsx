c118791943058914f1b8124cd0629d9ad9b29225
Aroa <aroa.rdtingenieros@gmail.com>
2026-04-08 08:36:58 +0200

    añadido hover también en EditorToolbar

--- frontend/src/modules/organizarScada/components/canvas/EditorToolbar.jsx
+++ frontend/src/modules/organizarScada/components/canvas/EditorToolbar.jsx
@@ -11,7 +11,7 @@ export default function EditorToolbar({
     <div className="flex items-center px-3 py-2 border-b bg-white">
 
       {/* Sidebar */}
-      <button onClick={onToggleSidebar} className="p-2 hover:bg-slate-100 rounded">
+      <button onClick={onToggleSidebar} className="cursor-pointer p-2 hover:bg-slate-100 rounded">
         {sidebarOpen ? <PanelLeftClose size={16} /> : <PanelLeftOpen size={16} />}
       </button>
 
@@ -20,7 +20,7 @@ export default function EditorToolbar({
       {/* Play */}
       <button
         onClick={onToggleLive}
-        className={`px-3 py-1 rounded-lg text-white transition-all duration-200 hover:scale-[1.05] active:scale-95 ${
+        className={` cursor-pointer px-3 py-1 rounded-lg text-white transition-all duration-200 hover:scale-[1.05] active:scale-95 ${
           isLiveMode
             ? "bg-red-500 hover:bg-red-600"
             : "bg-green-500 hover:bg-green-600"
